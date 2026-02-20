7992d00a9eba191fb2ab1fa8d573e3e122d2fea7
Sergio Belmar <sbelmar@gmail.com>
2026-02-20 13:59:46 -0300

    backup

--- demos/WUIForm-fillstyle/README.md
+++ demos/WUIForm-fillstyle/README.md
@@ -8,3 +8,10 @@ This demo demonstrates the use of WUIForm with fill style and native HTML inputs
 
 - WUIIcon - 0.1
 - WUIForm - 0.3
+- WUISelectpicker - 0.2
+- WUIDatepicker - 0.2
+- WUITimepicker - 0.2
+- WUIColorpicker - 0.2
+- WUISwitch - 0.3
+- WUIIntencity - 0.1
+- WUIButton - 0.2
--- demos/WUIForm-fillstyle/main.js
+++ demos/WUIForm-fillstyle/main.js
@@ -1,11 +1,49 @@
 const init = () => {
 	const form = new WUIForm({
 		selector: ".wui-form.my-form",
-		submit: false,
-		onScrolling: (top) => { },
-		onSubmit: () => { }
+		submit: false
+	});
+	const selectpicker = new WUISelectpicker({
+		selector: ".wui-selectpicker.my-selectpicker",
+		value: "2"
+	});
+	const datepicker = new WUIDatepicker({
+		selector: ".wui-datepicker.my-datepicker",
+		value: "2026-02-20"
+	});
+	const timepicker = new WUITimepicker({
+		selector: ".wui-timepicker.my-timepicker",
+		value: "12:00"
+	});
+	const colorpicker = new WUIColorpicker({
+		selector: ".wui-colorpicker.my-colorpicker",
+		value: "#ff0000"
+	});
+	const switchbox = new WUISwitch({
+		selector: ".wui-switch.my-switch",
+		value: "1"
+	});
+	const intencity = new WUIIntencity({
+		selector: ".wui-intencity.my-intencity",
+		value: "1"
+	});
+	const cancelButton = new WUIButton({
+		selector: ".wui-button.cancel",
+		onClick: () => { }
+	});
+	const submitButton = new WUIButton({
+		selector: ".wui-button.submit",
+		onClick: () => { }
 	});
 	form.init();
+	selectpicker.init();
+	datepicker.init();
+	timepicker.init();
+	colorpicker.init();
+	switchbox.init();
+	intencity.init();
+	cancelButton.init();
+	submitButton.init();
 }
 
 window.addEventListener("DOMContentLoaded", init);
