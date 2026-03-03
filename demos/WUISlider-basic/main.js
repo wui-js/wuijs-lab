e8170dcde2e07ee941c8be434752adf1aa9422e3
Sergio Belmar <sbelmar@gmail.com>
2026-03-02 23:15:40 -0300

    backup

--- demos/WUISlider-basic/main.js
+++ demos/WUISlider-basic/main.js
@@ -1,4 +1,6 @@
 const init = () => {
+	const prevButton = document.body.querySelector(".my-button.prev");
+	const nextButton = document.body.querySelector(".my-button.next");
 	const output = document.body.querySelector(".my-output");
 	const slider = new WUISlider({
 		selector: ".wui-slider.my-slider",
@@ -7,6 +9,12 @@ const init = () => {
 		}
 	});
 	slider.init();
+	prevButton.addEventListener("click", () => {
+		slider.prev();
+	});
+	nextButton.addEventListener("click", () => {
+		slider.next();
+	});
 }
 
 window.addEventListener("DOMContentLoaded", init);
