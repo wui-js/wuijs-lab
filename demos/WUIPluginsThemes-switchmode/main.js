879345e1891154cef8217e18189800ed3fe414dc
Sergio Belmar <sbelmar@gmail.com>
2026-03-05 13:18:22 -0300

    backup

--- demos/WUIPluginsThemes-switchmode/main.js
+++ demos/WUIPluginsThemes-switchmode/main.js
@@ -2,6 +2,13 @@ const wuiComponents = {};
 
 const init = () => {
 	const themes = new WUIPluginThemes();
+	wuiComponents.switch = new WUISwitch({
+		selector: ".wui-switch.my-switch",
+		activated: false,
+		onChange: (value, activated) => {
+			themes.setScheme(activated ? "dark" : "light");
+		}
+	});
 	wuiComponents.selectpicker = new WUISelectpicker({
 		selector: ".wui-selectpicker.my-selectpicker",
 		value: "2"
@@ -9,7 +16,6 @@ const init = () => {
 	Object.values(wuiComponents).forEach((component) => {
 		component.init();
 	});
-	themes.setScheme("dark");
 }
 
 window.addEventListener("wuiLoad", init);
